1c1b7edbcabd3e2ed0130bf46e51ad9a672891be
Mr. Beattie <mbeattie@beattietech.local>
2026-09-08 10:40:27 -0400

    chore: remove scratch images and coordinate probe scripts

--- scripts/extract_first.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-from PIL import Image
-
-with open("motherboard_0b78809.jpg", "wb") as f:
-    subprocess.run(["git", "show", "0b78809:public/images/hardware/motherboard-atx-landmarks.jpg"], stdout=f)
-
-img = Image.open("motherboard_0b78809.jpg")
-print("0b78809 image size:", img.size)
